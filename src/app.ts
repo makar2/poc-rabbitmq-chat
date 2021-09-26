159928b07adccd32b887457072e878d1616249d2
Makar <makar.melikyan@gmail.com>
2021-09-26 01:18:37 +0000

    Hello World through RabbitMQ

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import readline from 'readline';
 import dotenvFlow from 'dotenv-flow';
+import amqp, { Message } from 'amqplib/callback_api';
 import { TEACHERS } from '@/common/constants';
 
 dotenvFlow.config();
 
 /**
- * Set user's name and preferred channel
+ * Set user's name and preferred chat
  */
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,15 +14,48 @@ const rl = readline.createInterface({
 });
 
 let id: string | null = process.env.ID || null;
-let channel: string | null = process.env.CHANNEL || null;
+let chat: string | null = process.env.CHAT || null;
 
-if (!id) rl.question('What is your ID? ', (i: string) => { id = i; });
+if (!id) {
+  // eslint-disable-next-line brace-style
+  rl.question('What is your ID? ', (i: string) => { id = i; });
+}
 
-if (!channel) rl.question('What channel do you want to access? ', (ii: string) => { channel = ii; });
+if (!chat) {
+  rl.question('What chat do you want to access? ', (ii: string) => {
+    chat = ii;
+  });
+}
 
 const name: string | undefined = TEACHERS.find(el => el.id === id)?.name;
 
-if (name && channel) console.info(`Welcome back ${name}! Connecting to ${channel} channel.`);
+if (name && chat) console.info(`Welcome back ${name}! Connecting to ${chat} channel.`);
 else throw new Error('Unable to access');
 
 rl.close();
+
+/**
+ * Subscribe to chanel selected by teacher
+ */
+amqp.connect('amqp://guest:guest@rabbitmq:5672', (error0, connection) => {
+  console.log('>>> Connecting to RabbitMQ...');
+
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((error1, channel) => {
+    if (error1) throw error1;
+
+    const queue = 'hello-world';
+
+    channel.assertQueue(queue, { durable: false });
+
+    channel.consume(queue, (msg: Message | null) => {
+      const { message } = msg && JSON.parse(msg.content.toString());
+      console.log(message);
+    }, {
+      noAck: true,
+    });
+  });
+});

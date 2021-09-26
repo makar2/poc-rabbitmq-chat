cbf0abead963bcc7bd8e8341aeecd30a1eb711d9
Makar <makar.melikyan@gmail.com>
2021-09-26 23:06:59 +0000

    fanout chat created

--- src/app.ts
+++ src/app.ts
@@ -16,9 +16,8 @@ const rl = readline.createInterface({
 /**
  * Connect to RabbitMQ
  */
-const queue = 'hello-world';
 amqp.connect('amqp://guest:guest@rabbitmq:5672', (error0, connection) => {
-  console.log('>>> Connecting to RabbitMQ...');
+  console.info('>>> Connecting to RabbitMQ...');
 
   if (error0) {
     throw error0;
@@ -26,37 +25,60 @@ amqp.connect('amqp://guest:guest@rabbitmq:5672', (error0, connection) => {
 
   connection.createChannel((error1, channel) => {
     if (error1) throw error1;
-    channel.assertQueue(queue, { durable: false });
 
-    let id: string | null = process.env.ID || null;
+    let id: string | undefined = process.env.ID;
     let name: string | undefined;
-    let chat: string | null = process.env.CHAT || null;
+    let chat: string | undefined = process.env.CHAT;
 
     if (!id) {
       rl.question('What is your ID? ', (i: string) => {
         id = i;
         if (!chat) {
-          rl.question('What chat do you want to access? ', (ii: string) => {
-            chat = ii;
-            name = TEACHERS.find(el => el.id === id)?.name;
-            if (name && chat) console.info(`Welcome back ${name}! Connecting to ${chat} channel.`);
-            else throw new Error('Unable to log in');
+          rl.question('What chat do you want to access -- general or house? ', (ii: string) => {
+            const obj = TEACHERS.find(el => el.id === id);
+            if (obj) {
+              name = obj.name;
+              if (ii === 'house') chat = obj.house;
+              else if (ii === 'general') chat = 'general';
+              if (chat) console.info(`Welcome back ${name}! Connecting to ${chat} channel...`);
+
+              else throw new Error('Unable to log in: Chat not found');
+            }
+            else throw new Error('Unable to log in: User ID not found');
 
             /**
              * Subscribe to chanel selected by teacher
              */
-            channel.consume(queue, (msg: Message | null) => {
-              const { message } = msg && JSON.parse(msg.content.toString());
-              console.log(`\n<<< ${message}`);
-            }, {
-              noAck: true,
-            });
-            // add Messaging to Channel
+            channel.assertExchange(chat, 'fanout', { durable: false });
 
+            channel.assertQueue('', {
+              exclusive: true,
+            }, (error2, q) => {
+              if (error2) {
+                throw error2;
+              }
+              channel.bindQueue(q.queue, chat || 'general', '');
+
+              channel.consume(q.queue, (msg: Message | null) => {
+                const { author, exchange, message } = msg && JSON.parse(msg.content.toString());
+                console.info(`\n<<< [${exchange}] ${author === name ? 'You' : author}: ${message}`);
+              }, {
+                noAck: true,
+              });
+            });
+
+            /**
+             * Send a message
+             */
             const looper = () => {
               rl.question('>>>', (messageOut: string) => {
                 if (messageOut) {
-                  channel.sendToQueue(queue, Buffer.from(JSON.stringify({ message: messageOut })));
+                  const output = {
+                    author: name,
+                    exchange: chat,
+                    message: messageOut,
+                  };
+                  channel.publish(chat || 'general', '', Buffer.from(JSON.stringify(output)));
                   looper();
                 }
               });

4065080d0146d2bc75038f298b342b2e8c555ad7
Makar <makar.melikyan@gmail.com>
2021-09-26 04:52:51 +0400

    eslint upadted, .env primitive read from terminal added to select user and chanel

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+import readline from 'readline';
+import dotenvFlow from 'dotenv-flow';
+import { TEACHERS } from '@/common/constants';
+
+dotenvFlow.config();
+
+/**
+ * Set user's name and preferred channel
+ */
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let id: string | null = process.env.ID || null;
+let channel: string | null = process.env.CHANNEL || null;
+
+if (!id) rl.question('What is your ID? ', (i: string) => { id = i; });
+
+if (!channel) rl.question('What channel do you want to access? ', (ii: string) => { channel = ii; });
+
+const name: string | undefined = TEACHERS.find(el => el.id === id)?.name;
+
+if (name && channel) console.info(`Welcome back ${name}! Connecting to ${channel} channel.`);
+else throw new Error('Unable to access');
+
+rl.close();
---
+++ src/common/constants.ts
@@ -0,0 +1,69 @@
+export const HOUSES = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw'];
+
+export const TEACHERS = [
+  {
+    id: '01',
+    name: 'Barty Crouch Jr.',
+    house: 'Slytherin',
+  },
+  {
+    id: '02',
+    name: 'Dolores Umbridge',
+    house: 'Slytherin',
+  },
+  {
+    id: '03',
+    name: 'Quirinus Quirrell',
+    house: 'Ravenclaw',
+  },
+  {
+    id: '04',
+    name: 'Gilderoy Lockhart',
+    house: 'Ravenclaw',
+  },
+  {
+    id: '05',
+    name: 'Horace Slughorn',
+    house: 'Slytherin',
+  },
+  {
+    id: '06',
+    name: 'Rubeus Hagrid',
+    house: 'Gryffindor',
+  },
+  {
+    id: '07',
+    name: 'Sybill Trelawney',
+    house: 'Ravenclaw',
+  },
+  {
+    id: '08',
+    name: 'Severus Snape',
+    house: 'Slytherin',
+  },
+  {
+    id: '09',
+    name: 'Remus Lupin',
+    house: 'Gryffindor',
+  },
+  {
+    id: '10',
+    name: 'Pomona Sprout',
+    house: 'Hufflepuff',
+  },
+  {
+    id: '11',
+    name: 'Filius Flitwick',
+    house: 'Ravenclaw',
+  },
+  {
+    id: '12',
+    name: 'Albus Dumbledore',
+    house: 'Gryffindor',
+  },
+  {
+    id: '13',
+    name: 'Minerva Mcgonagall',
+    house: 'Gryffindor',
+  },
+];
